ee7ee4f8597d9b6f18e5a2bb1905ea3937825a13
unknown <singhrathoreayush824@gmail.com>
2025-08-10 21:41:00 +0530

    Test2

--- backend/app/routes/jobs.routes.js
+++ backend/app/routes/jobs.routes.js
@@ -4,14 +4,13 @@ import multer from 'multer';
 import { createJob, getAppliedJobs, listJob, getJobById, matchJobsFromResume, applyToJob, getApplicants  } from '../controllers/job.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
 
-// Configure multer for in-memory file storage
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const router = Router();
 
 router.post('/create', createJob);
-router.get('/applied', getAppliedJobs);
+router.get('/applied', authMiddleware, getAppliedJobs);
 router.get('/list', listJob);
 router.get('/:id/applicants', authMiddleware, getApplicants);
 router.get('/:id', getJobById)
