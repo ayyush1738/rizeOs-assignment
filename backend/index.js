0b38b67f9288952b720b8ab687a8a3bb3179cfda
unknown <singhrathoreayush824@gmail.com>
2025-08-10 22:11:44 +0530

    Ayush

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://rize-os-assignment-740aqlfxo-ayyush1738s-projects.vercel.app'],
+    origin: ['http://localhost:3000', 'https://rize-os-assignment-740aqlfxo-ayyush1738s-projects.vercel.app', 'rize-os-assignment-neon.vercel.app'],
     credentials: true,
 }));
 
